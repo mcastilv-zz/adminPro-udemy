3871733b0492707948b0dbaece04fa56bdd4db51
mcastilv <mcastilv@hotmail.com>
2018-01-18 18:58:03 -0300

    Sección 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -18,6 +18,8 @@ import { PAGES_ROUTES } from './pages.routes';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 @NgModule({
@@ -28,7 +30,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Graficas1Component,
     IncrementadorComponent,
     GraficoDonaComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent
   ],
   exports: [
     PagesComponent,
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  pagina: String = '';
+
+  constructor(
+    private router: Router,
+    public title: Title,
+    public meta: Meta) {
+    this.getDataRoute()
+      .subscribe(data => {
+        this.pagina = data.titulo;
+        this.title.setTitle(this.pagina);
+
+        let metaTag: MetaDefinition = {
+          name: 'description',
+          content: this.pagina
+        };
+
+        this.meta.updateTag(metaTag);
+
+      });
+  }
+
+  getDataRoute() {
+    return this.router.events
+      .filter(evento => evento instanceof ActivationEnd)
+      .filter((evento: ActivationEnd) => evento.snapshot.firstChild === null)
+      .map((evento: ActivationEnd) => evento.snapshot.data);
+  }
 
   ngOnInit() {
   }
-
 }
